0547c415d68019131c4eecc4bb9b945c0b6774b2
Administrator <Administrator@root>
2026-03-16 00:37:09 +0800

    feat(service): SettingsService 添加房型配置方法

--- src/main/java/com/hotel/service/SettingsService.java
+++ src/main/java/com/hotel/service/SettingsService.java
@@ -1,9 +1,18 @@
 package com.hotel.service;
 
+import com.hotel.dto.RoomTypeConfig;
 import com.hotel.dto.SettingsRequest;
 import com.hotel.dto.SettingsResponse;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 public interface SettingsService {
     SettingsResponse getSettings();
     SettingsResponse updateSettings(SettingsRequest request, String updatedBy);
+
+    Map<String, RoomTypeConfig> getRoomTypesConfig();
+
+    @Transactional
+    Map<String, RoomTypeConfig> updateRoomTypesConfig(Map<String, RoomTypeConfig> config);
 }
