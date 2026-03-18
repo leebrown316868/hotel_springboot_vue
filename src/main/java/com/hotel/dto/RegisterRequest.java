647ed559c3dc36d63cdea2b4a90dda79291e7e46
Administrator <Administrator@root>
2026-03-18 17:10:43 +0800

    feat(dto): 更新 RegisterRequest 添加 phone, country 字段和验证规则

--- src/main/java/com/hotel/dto/RegisterRequest.java
+++ src/main/java/com/hotel/dto/RegisterRequest.java
@@ -4,20 +4,32 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 public class RegisterRequest {
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+    @NotBlank(message = "姓名不能为空")
+    @Size(min = 2, max = 100, message = "姓名长度必须在2-100字符之间")
+    private String name;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 8, message = "密码长度不能少于8位")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+             message = "密码必须包含大小写字母和数字")
     private String password;
 
-    @NotBlank(message = "Name is required")
-    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
-    private String name;
+    @NotBlank(message = "电话不能为空")
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$",
+             message = "电话号码格式不正确")
+    private String phone;
+
+    @NotBlank(message = "国家不能为空")
+    @Size(min = 1, max = 50, message = "国家名称长度必须在1-50字符之间")
+    private String country;
 }
